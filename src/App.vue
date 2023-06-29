b7309831a73d3c5f4b21564352064ca7e2a9c4c5
NctZyc <2058443293@qq.com>
2023-06-29 15:29:35 +0800

    首页模块

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="flex-container">
+    <LayoutBlocak v-for="item in data" :key="item.title" v-bind:title="item.title" v-bind:width="item.width" v-bind:height="item.height"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LayoutBlocak from './components/LayoutBlock.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      data: [
+        { title: '天才专场', width: '100%', height: '10%'},
+        { title: 'test1', width: '30%', height: '20%'},
+        { title: 'test2', width: '30%', height: '20%'},
+        { title: 'test3', width: '30%', height: '20%'},
+        { title: 'test4', width: '30%', height: '20%'},
+        { title: 'test5', width: '30%', height: '20%'},
+        { title: 'test6', width: '30%', height: '20%'},
+        { title: 'test7', width: '30%', height: '20%'},
+        { title: 'test8', width: '30%', height: '20%'},
+        { title: 'test9', width: '30%', height: '20%'}
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    LayoutBlocak
   }
 }
 </script>
@@ -21,6 +38,37 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#flex-container{
+  display: -webkit-flex;
+  display: flex;
+  /*-webkit-justify-content: space-between;
+  justify-content: space-between;*/
+  -webkit-justify-content: space-around;
+  justify-content: space-around;
+  flex-wrap:wrap;
+  width: 100%;
+  height: 100vh;
+  background-color: lightgrey;
+}
+
+::-webkit-scrollbar {
+  width: 0px;
+}
+ 
+::-webkit-scrollbar-track {
+  background-color: none;
+}
+ 
+::-webkit-scrollbar-thumb {
+  background-color: none;
+}
+ 
+::-webkit-scrollbar-thumb:hover {
+  background-color: none;
+}
+ 
+::-webkit-scrollbar-thumb:active {
+  background-color: none;
 }
 </style>
---
+++ src/components/LayoutBlock.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-bind:style="flex_item"><p>{{ title }}</p></div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutBlock',
+  props: {
+    title: String,
+    width: String,
+    height: String,
+  },
+  computed: {
+    flex_item() {
+      return {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: this.width,
+            height: this.height,
+            backgroundColor: 'blue',
+            textAlign: 'center'
+          }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
